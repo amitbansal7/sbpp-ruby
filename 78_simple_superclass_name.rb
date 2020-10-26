9502064536715fedfa030c959e99e559261542b4
Amit Bansal <007amitbansal@gmail.com>
2020-10-27 00:05:12 +0530

    classes

---
+++ 78_simple_superclass_name.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+# What do you call a class that is expected to the the root of an inheritance hierarchy?
+
+# Name a superclass with a single word that conveys its purpose in the design.
+
+#   Number
+#   Collection
+#   Magnitude
+#   Model
---
+++ 79_qualified_subclass_name.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+# What do you name a new subclass?
+
+# Name subclass in your hierarchies by prepending an adjective
+# to the superclass name.
